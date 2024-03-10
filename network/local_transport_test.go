10b4efc3f377cc8d0a1c9158ef800d01b6225388
ivnhk <iandrc9@gmail.com>
2024-03-10 16:25:34 +0100

    fix: Fix test for local transport

--- network/local_transport_test.go
+++ network/local_transport_test.go
@@ -12,8 +12,8 @@ func TestConnect(t *testing.T) {
 	tra.Connect(trb)
 	trb.Connect(tra)
 
-	assert.Equal(t, tra.peers[trb.Addr()], trb)
-	assert.Equal(t, trb.peers[tra.Addr()], tra)
+	assert.Equal(t, tra.(*LocalTransport).peers[trb.Addr()], trb)
+	assert.Equal(t, trb.(*LocalTransport).peers[tra.Addr()], tra)
 }
 
 func TestSendMessage(t *testing.T) {
